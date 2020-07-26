c56db39b281e27947324a42b041d1e9fa2d8a8ab
Haocheng Hu <hc.hu@utoronto.ca>
2020-07-26 16:19:47 -0400

    print python version number

--- random_code_snippets/timing_template.py
+++ random_code_snippets/timing_template.py
@@ -13,6 +13,7 @@ def func2():
 
 if __name__ == '__main__':
     from timeit import timeit
+    from sys import version
 
     reps = 1
     func1_name = 'func1'
@@ -20,6 +21,8 @@ if __name__ == '__main__':
 
     time1 = timeit('func1()', number=reps, globals=globals())
     time2 = timeit('func2()', number=reps, globals=globals())
+
+    print(f'Python version {version}\n')
 
     print(f'Value of {func1_name}/{func2_name}: {time1 / time2:.4f}')
     print(f'Time for {func1_name}: {time1}\nTime for {func2_name}: {time2}')
