47c8387ae861a16a71933b29bd3ee4fb14cef32a
Haocheng Hu <hc.hu@utoronto.ca>
2020-09-14 13:07:56 -0400

    add course_sorter.py

---
+++ random_code_snippets/course_sorter.py
@@ -0,0 +1,8 @@
+courses = ['Algorithms and Data Structures', 'Software Engineering and Design',
+           'Programming Languages and Techniques', 'Discrete Mathematics',
+           'Theory of Computation', 'Systems Programming', 'Databases',
+           'Computer Organization', 'Machine Learning', 'Operating Systems']
+
+if __name__ == '__main__':
+    courses.sort()
+    print(', '.join(courses))
