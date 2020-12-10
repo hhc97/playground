3dab49d264202e2f5e04227be2a716aee1439865
Haocheng Hu <hc.hu@utoronto.ca>
2020-12-09 22:13:51 -0500

    fd get closures

--- database_utils/relation.py
+++ database_utils/relation.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from fd import FD
 
 
@@ -24,7 +26,32 @@ class Relation:
         fds = ', '.join(sorted(str(fd) for fd in self.fds))
         return f'Relation: {rel}\nFDs: {fds}'
 
+    def _closure(self, curr_fd: FD) -> str:
+        """
+        Returns the closure of a FD.
+        """
+        closure = curr_fd.determinants.copy()
+        running = True
+        while running:
+            running = False
+            for fd in self.fds:
+                if fd.determinants.issubset(closure) and not fd.dependents.issubset(closure):
+                    closure.update(fd.dependents)
+                    running = True
+        return ''.join(sorted(list(closure)))
+
+    def get_closures(self) -> str:
+        """
+        Gets the closures of all FDs in the relation.
+        """
+        closures = ['Closures:']
+        for fd in self.fds:
+            closures.append(f'{fd.get_determinants()}+ = {self._closure(fd)}')
+        return '\n'.join(closures)
+
 
 if __name__ == '__main__':
     test = Relation('relation_input.txt')
     print(test)
+    print()
+    print(test.get_closures())
