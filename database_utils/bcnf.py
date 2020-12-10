8167924ead3ac556d9bbfcfd06cc6b1b36e3efac
Haocheng Hu <hc.hu@utoronto.ca>
2020-12-09 22:47:01 -0500

    bcnf basic loop

--- database_utils/bcnf.py
+++ database_utils/bcnf.py
@@ -1,4 +1,18 @@
+from __future__ import annotations
+
 from fd import FD
+from relation import Relation
+
+
+def bcnf(rel: Relation) -> list:
+    for fd in rel.fds:
+        if rel.closure(fd) != rel.relation:
+            pass
+    return [rel]
+
 
 if __name__ == '__main__':
-    pass
+    relation = Relation('relation_input.txt')
+    print(relation)
+    print(relation.get_closures())
+    test = bcnf(relation)
