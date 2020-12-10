226fae3a9ed28d8c5db6e97bff933d6149a4b34f
Haocheng Hu <hc.hu@utoronto.ca>
2020-12-09 21:37:12 -0500

    relation init and str

--- database_utils/relation.py
+++ database_utils/relation.py
@@ -1,9 +1,30 @@
+from fd import FD
+
+
 class Relation:
     """
     A class that represents a relation and it's FDs.
     """
-    pass
+
+    def __init__(self, relation_file: str) -> None:
+        """
+        Parses the relation file to initialize the class properly.
+        """
+        self.fds = []
+        with open(relation_file, 'r') as f:
+            self.relation = set(f.readline().strip())
+            for line in f:
+                self.fds.append(FD(line))
+
+    def __str__(self) -> str:
+        """
+        Returns the string representation of this relation.
+        """
+        rel = ''.join(sorted(list(self.relation)))
+        fds = ', '.join(sorted(str(fd) for fd in self.fds))
+        return f'Relation: {rel}\nFDs: {fds}'
 
 
 if __name__ == '__main__':
-    pass
+    test = Relation('relation_input.txt')
+    print(test)
