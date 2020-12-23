6c728ae108d84ef8b115526d33d1faa0770d8610
Haocheng Hu <hc.hu@utoronto.ca>
2020-12-23 14:08:53 -0500

    update course sorter

--- random_code_snippets/course_sorter.py
+++ random_code_snippets/course_sorter.py
@@ -1,8 +1,14 @@
-courses = ['Algorithms and Data Structures', 'Software Engineering and Design',
-           'Programming Languages and Techniques', 'Discrete Mathematics',
-           'Theory of Computation', 'Systems Programming', 'Databases',
-           'Computer Organization', 'Machine Learning', 'Operating Systems']
-
-if __name__ == '__main__':
+def print_sorted() -> None:
+    """
+    Reads courses from courses.txt and prints out a sorted version.
+    """
+    courses = []
+    with open('courses.txt', 'r') as f:
+        for line in f:
+            courses.append(line.strip())
     courses.sort()
     print(', '.join(courses))
+
+
+if __name__ == '__main__':
+    print_sorted()
