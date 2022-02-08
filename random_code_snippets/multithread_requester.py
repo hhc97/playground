a81d1264c807fb666fb15b58c0a1c7f6c7662122
Haocheng Hu <hhu@zynga.com>
2022-02-08 13:01:13 -0500

    remove logging for requester

--- random_code_snippets/multithread_requester.py
+++ random_code_snippets/multithread_requester.py
@@ -6,14 +6,12 @@ import requests
 def get_stuff(name: str):
     for i in range(20):
         r = requests.get('https://spatial-logs.herokuapp.com/test/refresh')
-        print(f'thread {name} did request {i}')
-        with open('logs.txt', 'a') as f:
-            f.write(r.text)
+        print(f'thread {name} did request {i} (response length: {len(r.text)})')
 
 
 if __name__ == '__main__':
     threads = []
-    for t in range(6):
+    for t in range(8):
         thread = threading.Thread(target=get_stuff, args=(t,))
         threads.append(thread)
         thread.start()
