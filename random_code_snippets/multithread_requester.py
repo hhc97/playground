2823557fce6daf2199e8be2d401e9d5db7dacafd
Haocheng Hu <hc.hu@utoronto.ca>
2022-04-25 10:44:32 -0400

    fix simultaneous print

--- random_code_snippets/multithread_requester.py
+++ random_code_snippets/multithread_requester.py
@@ -4,17 +4,17 @@ from time import time
 import requests
 
 
-def get_stuff(name: str):
+def do_request(name: str):
     for i in range(20):
         start = time()
         r = requests.get('https://google.com/robots.txt')
-        print(f'thread {name} did request {i} (response length: {len(r.text)}) {time() - start}s')
+        print(f'thread {name} did request {i} (response length: {len(r.text)}) {time() - start:.5f}s\n', end='')
 
 
 if __name__ == '__main__':
     threads = []
     for t in range(8):
-        thread = threading.Thread(target=get_stuff, args=(t,))
+        thread = threading.Thread(target=do_request, args=(t,))
         threads.append(thread)
         thread.start()
     for thread in threads:
